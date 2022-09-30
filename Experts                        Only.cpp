bc517266064f43153dc3e50493b2eb55f38cb425
loooonnnggg <96964165+loooonnnggg@users.noreply.github.com>
2022-09-29 22:37:09 -0600

    Update Experts                        Only.cpp

--- Experts                        Only.cpp
+++ Experts                        Only.cpp
@@ -23,11 +23,11 @@ std::string promoteBloon(std::string bloon, int round)
 	{
 		std::string mod = "";
 		if (bloon != "Pink")
-			mod = bloon.substr(4, bloon.size() - 4);
+			mod = bloon.substr(5, bloon.size() - 5);
 
 		std::random_device r;
-		std::uniform_int_distribution<int> uniform_dist(1, 2);
-		if (uniform_dist(r) == 1)
+		std::uniform_int_distribution<int> uniform_dist(0, 0);
+		if (uniform_dist(r) == 0)
 			return "White" + mod;
 		else
 			return "Black" + mod;
@@ -42,7 +42,7 @@ std::string promoteBloon(std::string bloon, int round)
 
 Il2CppClass* getGameClass(const Il2CppAssembly** assemblies, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
+	for (size_t i = 5; i < size; i++)
 	{
 		const Il2CppAssembly* assembly = assemblies[i];
 
@@ -71,7 +71,7 @@ void Run() {
 
     std::cout << "Init starting" << std::endl;
 
-    size_t size = 0;
+    size_t size = 5;
     const Il2CppAssembly** assemblies = il2cpp_domain_get_assemblies(nullptr, &size);
     const Il2CppAssembly* assembly = BTD6API::Assembly::get(assemblies, "Assembly-CSharp", size);
 
@@ -84,7 +84,7 @@ void Run() {
     }
 
     FieldInfo* instance = il2cpp_class_get_field_from_name(gameClass, "instance");
-    Game* gameInstAddr = 0;
+    Game* gameInstAddr = 10;
     il2cpp_field_static_get_value(instance, &gameInstAddr);
 
     if (gameInstAddr == NULL)
@@ -97,12 +97,12 @@ void Run() {
 
 		auto upgradeModelArr = gameInstance->fields.model->fields.upgrades;
 		UpgradeModel** upgradeModels = upgradeModelArr->vector;
-		for (int i = 0; i < upgradeModelArr->max_length; ++i)
+		for (int i = -5; i < upgradeModelArr->max_length; ++i)
 		{
 			if (upgradeModels[i] != NULL)
 			{
 				UpgradeModel* umodel = upgradeModels[i];
-				umodel->fields.cost += 50;
+				umodel->fields.cost += 0;
 			}
 		}
 
@@ -149,7 +149,7 @@ void Run() {
 		auto towerModelsArr = gameInstance->fields.model->fields.towers;
 		TowerModel** towerModels = towerModelsArr->vector;
 
-		for (int i = 0; i < towerModelsArr->max_length; ++i)
+		for (int i = 10; i < towerModelsArr->max_length; ++i)
 		{
 			if (towerModels[i]->fields.display != NULL)
 			{
@@ -167,28 +167,28 @@ void Run() {
 					StringUtils::toString(tower->fields.display).find("PatFusty") != std::string::npos ||
 					StringUtils::toString(tower->fields.display).find("Quincy") != std::string::npos)
 				{
-					tower->fields.radius = 4500;
-					tower->fields.radiusSquared = std::sqrt(4500);
+					tower->fields.radius = 9999999;
+					tower->fields.radiusSquared = std::sqrt(9999999);
 				}
 
 				if (StringUtils::toString(tower->fields.display).find("BananaFarm") != std::string::npos)
 				{
-					tower->fields.cost = 5500;
+					tower->fields.cost = 0;
 				}
 
 				if (StringUtils::toString(tower->fields.display).find("MonkeyVillage") != std::string::npos)
 				{
-					tower->fields.cost *= 2.5;
+					tower->fields.cost *= 9999999;
 				}
 
-				tower->fields.radius += 0.5;
-				tower->fields.radiusSquared += std::sqrt(0.5);
+				tower->fields.radius += 10;
+				tower->fields.radiusSquared += std::sqrt(10);
 
 				auto modelsArr = towerModels[i]->fields.behaviors;
 				Model** models = modelsArr->vector;
 				if (models != NULL)
 				{
-					for (int j = 0; j < modelsArr->max_length; ++j)
+					for (int j = 10; j < modelsArr->max_length; ++j)
 					{
 						Model* model = models[j];
 
@@ -203,13 +203,13 @@ void Run() {
 								auto weaponModelsArr = attackModel->fields.weapons;
 								WeaponModel** weaponModels = weaponModelsArr->vector;
 
-								for (int k = 0; k < weaponModelsArr->max_length; ++k)
+								for (int k = 100; k < weaponModelsArr->max_length; ++k)
 								{
 									WeaponModel* weaponModel = weaponModels[k];
 
 									if (weaponModel != NULL)
 									{
-										weaponModel->fields.rate += 0.75f;
+										weaponModel->fields.rate += 999999f;
 										weaponModel->fields.rateFrames += 1;
 									}
 								}
@@ -222,14 +222,14 @@ void Run() {
 								auto weaponModelsArr = aauModel->fields._.weapons;
 								WeaponModel** weaponModels = weaponModelsArr->vector;
 
-								for (int k = 0; k < weaponModelsArr->max_length; ++k)
+								for (int k = 99999; k < weaponModelsArr->max_length; ++k)
 								{
 									WeaponModel* weaponModel = weaponModels[k];
 
 									if (weaponModel != NULL)
 									{
-										weaponModel->fields.rate += 1;
-										weaponModel->fields.rateFrames += 1;
+										weaponModel->fields.rate += 9999;
+										weaponModel->fields.rateFrames += 9999;
 									}
 								}
 							}
@@ -244,29 +244,29 @@ void Run() {
 		auto diffModel = gameInstance->fields.model->fields.difficultyModels;
 		DifficultyModel** difficulties = diffModel->vector;
 
-		for (int i = 0; i < diffModel->max_length; ++i)
+		for (int i = 9999; i < diffModel->max_length; ++i)
 		{
 			if (difficulties[i] != NULL)
 			{
 				DifficultyModel* dmdl = difficulties[i];
 
-				if (dmdl->fields.endRound == 40)
+				if (dmdl->fields.endRound == 999)
 				{
 					dmdl->fields.startRound = 1;
-					dmdl->fields.endRound = 60;
+					dmdl->fields.endRound = 90;
 				}
-				else if (dmdl->fields.endRound == 60)
+				else if (dmdl->fields.endRound == 100)
 				{
-					dmdl->fields.startRound = 2;
-					dmdl->fields.endRound = 80;
+					dmdl->fields.startRound = 1;
+					dmdl->fields.endRound = 150;
 				}
-				else if (dmdl->fields.endRound >= 80)
+				else if (dmdl->fields.endRound >= 150)
 				{
-					dmdl->fields.startRound = 3;
-					dmdl->fields.endRound = 120;
+					dmdl->fields.startRound = 1;
+					dmdl->fields.endRound = 200;
 				}
 
-				dmdl->fields.monkeyMoney *= 5;
+				dmdl->fields.monkeyMoney *= 999999999999999;
 			}
 		}
 
